cc3f20e3716f4f7413a1c367f5fba5f822be60a5
rebekajakob <rebusacka@gmail.com>
2023-01-10 13:01:07 +0100

    replace magic numbers

--- number-guessing.py
+++ number-guessing.py
@@ -1,5 +1,14 @@
 import random
 
+NOT_INTEGER_MESSAGE = "The input should be an integer NUMBER! "
+FROM_NUMBER_MESSAGE = "Please give the from number! "
+TO_NUMBER_MESSAGE = "Please give the to number! "
+SMALLER_FROM_MESSAGE = ["Please give a bigger number then ", "! "]
+VICTORY_MESSAGE = "You're right! the number is "
+SMALLER_MESSAGE = "Number is smaller!"
+BIGGER_MESSAGE = "The number is bigger!"
+GUESS_NUMBER_MESSAGE = "Please guess a number! "
+
 
 def get_valid_number(input_text):
     valid = False
@@ -10,36 +19,37 @@ def get_valid_number(input_text):
             if input_guess.count(',') != 0 or input_guess.count('.') != 0:
                 raise ValueError
         except ValueError:
-            input_guess = input("The input should be an integer NUMBER! ")
+            input_guess = input(NOT_INTEGER_MESSAGE)
         else:
             return int(input_guess)
 
 
 def get_random_number():
-    from_number = get_valid_number("Please give the from number! ")
-    to_number = get_valid_number("Please give the to number! ")
+    from_number = get_valid_number(FROM_NUMBER_MESSAGE)
+    to_number = get_valid_number(TO_NUMBER_MESSAGE)
     while to_number <= from_number:
-        to_number = get_valid_number("Please give a bigger number then " + str(from_number) + "! ")
+        to_number = get_valid_number(SMALLER_FROM_MESSAGE[0] + str(from_number) + SMALLER_FROM_MESSAGE[1])
     return random.randint(from_number, to_number)
 
 
 def print_victory_message(guess):
-    print("You're right! the number is " + str(guess))
+    print(VICTORY_MESSAGE + str(guess))
 
 
 def main_logic(guess, number_to_guess):
     while int(guess) != number_to_guess:
         if number_to_guess < int(guess):
-            print("Number is smaller!")
+            print(SMALLER_MESSAGE)
         else:
-            print("The number is bigger!")
-        guess = get_valid_number("Please guess a number! ")
+            print(BIGGER_MESSAGE)
+        guess = get_valid_number(GUESS_NUMBER_MESSAGE)
+    return guess
 
 
 def number_guessing():
     number_to_guess = get_random_number()
-    guess = get_valid_number("Please guess a number! ")
-    main_logic(guess, number_to_guess)
+    guess = get_valid_number(GUESS_NUMBER_MESSAGE)
+    guess = main_logic(guess, number_to_guess)
     print_victory_message(guess)
 
 
