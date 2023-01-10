1b894421a1e7c6062e45aa1f6cb104c50042f007
rebekajakob <rebusacka@gmail.com>
2023-01-10 12:37:46 +0100

    add check if to number is smaller than from and check if the given string contains a float

--- number-guessing.py
+++ number-guessing.py
@@ -7,6 +7,8 @@ def get_valid_number(input_text):
     while not valid:
         try:
             int(input_guess)
+            if input_guess.count(',') != 0 or input_guess.count('.') != 0:
+                raise ValueError
         except ValueError:
             input_guess = input("The input should be an integer NUMBER! ")
         else:
@@ -16,6 +18,8 @@ def get_valid_number(input_text):
 def get_random_number():
     from_number = get_valid_number("Please give the from number! ")
     to_number = get_valid_number("Please give the to number! ")
+    while to_number <= from_number:
+        to_number = get_valid_number("Please give a bigger number then " + str(from_number) + "! ")
     return random.randint(from_number, to_number)
 
 
