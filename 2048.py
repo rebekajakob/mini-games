0c2c32af8146dc4e11a4d9bd12f37c4b4ccf23ad
rebekajakob <rebusacka@gmail.com>
2023-01-11 09:51:51 +0100

    implement find empty places on board

--- 2048.py
+++ 2048.py
@@ -6,4 +6,15 @@ def create_empty_board():
     board = []
     for row in range(4):
         board.append([str(0)] * 4)
+    return board
+
+
+def get_empty_indexes(board):
+    empty_indexes = []
+    for index in range(16):
+        if board[index // 4][index % 4] == '0':
+            empty_indexes.append(index)
+    return empty_indexes
+
+
 
