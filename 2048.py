a999e385ab908e02f9f5042c922461f4ece6acdf
rebekajakob <rebusacka@gmail.com>
2023-01-11 10:14:17 +0100

    implement create starter board

--- 2048.py
+++ 2048.py
@@ -18,8 +18,8 @@ def get_empty_indexes(board):
 
 
 def get_place_for_random_number(empty_indexes):
-    random_index = random.randint(0, len(empty_indexes))
-    return random_index
+    random_index = random.randint(0, len(empty_indexes)-1)
+    return empty_indexes[random_index]
 
 
 def get_random_number(probability_of_two):
@@ -27,4 +27,15 @@ def get_random_number(probability_of_two):
     if random_number < probability_of_two:
         return '2'
     return '4'
+
+
+def create_starter_board():
+    empty_board = create_empty_board()
+    first_placement_chances = [100, 70]
+    for i in range(2):
+        empty_indexes = get_empty_indexes(empty_board)
+        random_place = get_place_for_random_number(empty_indexes)
+        random_number = get_random_number(first_placement_chances[i])
+        empty_board[random_place // 4][random_place % 4] = random_number
+    return empty_board
 
