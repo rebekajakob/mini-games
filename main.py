b28461cfc83f062e2cb1b817b1f1f041d61255c5
rebekajakob <rebusacka@gmail.com>
2023-01-19 10:16:23 +0100

    import 2048 and guessing game, and implement main run method

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import number_guessing_game
+import game_2048
+
 NUMBER_OF_GAMES = 2
 WELCOME_MESSAGE = "Hello, welcome to my little gaming program. I hope you will enjoy it!"
 CHOOSE_GAME_MESSAGE = "Choose from the games below!"
 MENU_POINTS = "1. Number guessing \n2. 2048"
 CHOOSE_NUMBER_MESSAGE = "Please choose a number! "
-WRONG_INPUT_MESSAGE = "Please choose a valid number"
+WRONG_INPUT_MESSAGE = "Please choose a valid number! "
 
 
 def welcome_message():
@@ -18,9 +21,22 @@ def menu():
     while not valid:
         try:
             int(input_guess)
-            if input_guess not in range(1, NUMBER_OF_GAMES + 1):
+            if int(input_guess) not in range(1, NUMBER_OF_GAMES + 1):
                 raise ValueError
         except ValueError:
             input_guess = input(WRONG_INPUT_MESSAGE)
         else:
             return int(input_guess)
+
+
+def start_game():
+    welcome_message()
+    get_game = menu()
+    if get_game == 1:
+        number_guessing_game.number_guessing()
+    else:
+        game_2048.run_game_2048()
+
+
+if __name__ == '__main__':
+    start_game()
